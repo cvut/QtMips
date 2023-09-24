1d9a9157aea2438517af87b38f3f65d6472d9951
Rutherther <rutherther@proton.me>
2023-09-24 16:50:32 +0200

    Make default.nix point to correct path

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@ let
   pkgs = import <nixpkgs> { };
 in
 {
-    qtmips = pkgs.libsForQt5.callPackage (import extras/packages/nix/qtmips.nix) {};
+    qtmips = pkgs.libsForQt5.callPackage (import extras/packaging/nix/qtmips.nix) {};
 }
